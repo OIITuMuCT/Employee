5b30a7d647bb61828a7e2228e299443ea501c643
OIITuMuCT <vataman@live.ru>
2024-09-06 22:24:30 +0300

    Course CRUD end

--- employee_learning/views.py
+++ employee_learning/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView,  DeleteView, DetailView, UpdateView
 
-# Create your views here.
+from .models import LearningCourse
+
+# Create CRUD Web app.
+class CourseList(ListView):
+    model = LearningCourse
+    template_name = 'employee_learning/course_list.html'
+    context_object_name = 'course_object_list'
+
+
+class CourseDetail(DetailView):
+    model = LearningCourse
+    template_name = 'employee_learning/course_detail.html'
+    context_object_name = 'course_object'
+    
+    # def get_context_data(self, **kwargs) -> dict[str, Any]:
+    #     context = super().get_context_data(**kwargs)
+    #     context[""] = 
+    #     return context
+    
+class CourseCreate(CreateView):
+    model = LearningCourse
+    template_name = 'employee_learning/course_create.html'
+    fields = ('title', 'level', 'employee')
+    success_url = reverse_lazy('course_list')
+
+
+class CourseUpdate(UpdateView):
+    model = LearningCourse
+    template_name = 'employee_learning/course_update.html'
+    fields = ('title', 'level', 'employee')
+    success_url = reverse_lazy('course_list')
+    
+class CourseDelete(DeleteView):
+    model = LearningCourse
+    template_name = 'employee_learning/course_delete.html'
+    success_url = reverse_lazy('course_list')
